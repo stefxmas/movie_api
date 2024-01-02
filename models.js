97583575c5b1be0644bcaff2e49d2ee31ee72c97
stefxmas <143958588+stefxmas@users.noreply.github.com>
2024-01-02 12:45:02 -0500

    Create models.js
    models 2.6

---
+++ models.js
@@ -0,0 +1,22 @@
+const mongoose = require('mongoose');
+
+
+let movieSchema = mongoose.Schema({
+    Title: {type: String, required: true},
+    Description: {type: String, required: true},
+    Genre: {
+        Name: String,
+        Description: String
+    },
+    Director: {
+        Name: String,
+        Bio: String
+    },
+    Actors: [String],
+    ImagePath: String,
+    Featured: Boolean
+});
+
+let Movie = mongoose.model('Movie', movieSchema);
+
+module.exports.Movie = Movie;
