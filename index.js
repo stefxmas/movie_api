adb54b819f01d86a67064120ff77c0dc6ed1177f
stefxmas <143958588+stefxmas@users.noreply.github.com>
2024-01-02 11:05:03 -0500

    update to index.js

--- index.js
+++ index.js
@@ -1,9 +1,57 @@
 // const express = require('express');
 // const app = express();
 
-const express = require('express'),
-  morgan = require('morgan');
+// const express = require('express'),
+//   morgan = require('morgan');
 
+const express = require('express'),
+    fs = require('fs'),
+    morgan = require('morgan'),
+    path = require('path');
+
+    let topTenMovies = [
+        {
+            title: 'The Godfather',
+            Director: 'Francis Ford Coppola'
+        },
+        {
+            title: 'The Godfather Part II',
+            Director: 'Francis Ford Coppola'
+        },
+        {
+            title: 'Schindler\' List',
+            Director: 'Steven Spielberg'
+        },
+        {
+            title: 'The Dark Knight',
+            Director: 'Christopher Nolan'
+        },
+        {
+            title: 'Forest Gump',
+            Director: 'Robert Zemeckis'
+        },
+        {
+            title: 'Pulp Fiction',
+            Director: 'Quentin Tarantino'
+        },
+        {
+            title: 'The Shawshank Redemption',
+            Director: 'Frank Darabont'
+        },
+        {
+            title: 'Inception',
+            Director: 'Christopher Nolan'
+        },
+        {
+            title: 'The Lion King',
+            Director: 'Rob Minkoff'
+        },
+        {
+            title: 'Black Panther',
+            Director: 'Ryan Coogler'
+        }
+    ]
+    
 const app = express();
 
 app.use(morgan('common'));
