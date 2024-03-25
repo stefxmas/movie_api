7ecb57a9cc175b15f691acef87c55285f738296d
stefxmas <143958588+stefxmas@users.noreply.github.com>
2024-03-24 21:26:55 -0400

    added cors
    added cors package

--- index.js
+++ index.js
@@ -9,6 +9,9 @@ const express = require('express');
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
+const cors = require('cors');
+app.use(cors());
+
 let auth = require('./auth')(app);
 const passport = require('passport');
 require('./passport');
