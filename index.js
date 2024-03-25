fdb9e48277a020935b746cb2031770a0d9b8c1b7
stefxmas <143958588+stefxmas@users.noreply.github.com>
2024-03-25 11:52:43 -0400

    update index.js
    added validation for users

--- index.js
+++ index.js
@@ -26,21 +26,45 @@ const Movies = Models.Movie;
 const Users = Models.User;
 
 //create
-app.post('/users', (req, res) => {
-  //let newUser = req.body;
+app.post('/users',[
+  check('Username', 'Username is required').isLength({min: 5}),
+  check('Username', 'Username contains non alphanumeric characters - not allowed.').isAlphanumeric(),
+  check('Password', 'Password is required').not().isEmpty(),
+  check('Email', 'Email does not appear to be valid').isEmail()
+], async (req, res) => {
+
+// check the validation object for errors
+  let errors = validationResult(req);
+
+  if (!errors.isEmpty()) {
+    return res.status(422).json({ errors: errors.array() });
+  }
   let hashedPassword = Users.hashPassword(req.body.Password);
-  Users.create({
-    Username: req.body.Username,
-    Password: hashedPassword,   // Hashed password
-    Email: req.body.Email,
-    Birthday: req.body.Birthday
-  })
-  .then((user)=> {
-    res.status(200).json(user);
-}).catch((error)=>  {
-    res.status(400).send('ERR')
-})
-})
+  await Users.findOne({ Username: req.body.Username }) // Search to see if a user with the requested username already exists
+    .then((user) => {
+      if (user) {
+      //If the user is found, send a response that it already exists
+        return res.status(400).send(req.body.Username + ' already exists');
+      } else {
+        Users
+          .create({
+            Username: req.body.Username,
+            Password: hashedPassword,
+            Email: req.body.Email,
+            Birthday: req.body.Birthday
+          })
+          .then((user) => { res.status(201).json(user) })
+          .catch((error) => {
+            console.error(error);
+            res.status(500).send('Error: ' + error);
+          });
+      }
+    })
+    .catch((error) => {
+      console.error(error);
+      res.status(500).send('Error: ' + error);
+    });
+});
 
 //update 
 app.put('/users/:Username', passport.authenticate('jwt', { session: false }), (req, res) => {
