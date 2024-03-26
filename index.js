7c500d3bac1a17a302186a22a5b75043a0845c84
stefxmas <143958588+stefxmas@users.noreply.github.com>
2024-03-26 11:32:57 -0400

    update index.js
    updated app.listen

--- index.js
+++ index.js
@@ -276,6 +276,7 @@ app.get('/secreturl', (req, res) => {
   res.send('This is a secret url with super top-secret content.');
 });
 
-app.listen(8080, () => {
-  console.log('Your app is listening on port 8080.');
+const port = process.env.PORT || 8080;
+app.listen(port, '0.0.0.0',() => {
+ console.log('Listening on Port ' + port);
 });
