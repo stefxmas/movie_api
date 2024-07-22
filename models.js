13956d5009952247279237ad8392342bde8020b7
stefxmas <143958588+stefxmas@users.noreply.github.com>
2024-07-22 14:41:23 -0400

    fixing models.js

--- models.js
+++ models.js
@@ -32,27 +32,14 @@ let movieSchema = mongoose.Schema({
 userSchema.methods.validatePassword = function(password) {
     return bcrypt.compareSync(password, this.Password);
 
-    let directorSchema = mongoose.Schema({
-      _id:{ type: mongoose.Schema.Types.ObjectId, ref: 'Director', required: true},
-      Name:{type: String, required: true},
-      Bio:{type: String, required: true}, 
-      birthDate: Date,
-      DeathDate: Date   
-  });
-  let genreSchema = mongoose.Schema({
-      _id:{ type: mongoose.Schema.Types.ObjectId, ref: 'Genre', required: true},
-      Name:{type: String, required: true},
-      Description:{type: String, required: true} 
-  });
+    
 
 
 };
   let Movie = mongoose.model('Movie', movieSchema);
   let User = mongoose.model('User', userSchema);
-  let Director = mongoose.model('Director', directorSchema);
-  let Genre = mongoose.model('Genre', genreSchema)
+  
   
   module.exports.Movie = Movie;
   module.exports.User = User;
-  module.exports.Director = Director;
-  module.exports.Genre = Genre;
+  
